7a71f53b08768d6ae97c98923003ee8bc76bf181
Manisha-kandel <64305372+Manisha-kandel@users.noreply.github.com>
2026-03-04 18:01:04 -0500

    revision two pointers

--- python/patterns/two_pointers/lc_0015_3sum.py
+++ python/patterns/two_pointers/lc_0015_3sum.py
@@ -23,7 +23,7 @@ class Solution:
             # print(i)
             if nums[i] > 0:
                 break
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:   #as the possible 3sum for the same number was already looked at
                 continue
 
             target = -(nums[i])
@@ -48,3 +48,42 @@ class Solution:
                     # break
         return res
 
+
+'''neater soln from revision'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #we aren't needed to maintain the indices, we just need to return the numbers that can add upto 0, so we are only concerned about that
+
+        nums = sorted(nums)
+        res = []
+        if nums[0] > 0:
+            return res
+        if nums[-1] < 0:
+            return res
+        
+        n = len(nums)
+        
+        for i in range(0, n-2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:     #avoid duplicate i to search for
+                continue
+            
+            target = -(nums[i])
+
+            l = i + 1
+            r = n -1
+
+            while l < r:
+                if nums[l] + nums[r] > target:
+                    r -= 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]: l += 1   #avoid duplicate i,l  (i.e. duplicate triplets)
+                    while l < r and nums[r] == nums[r+1]: r -= 1   #avoid duplicate i,r  (i.e. duplicate triplets)
+        
+        return res
